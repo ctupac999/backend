0a9a08de09b76e0501ba35a1bd6323c4752176c8
ctupac999 <cristiands9999@gmail.com>
2024-10-24 19:15:16 +0200

    backend ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { PrismaService } from './prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),  // Configura .env como global
+    ProductsModule,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
